8d668f5b91122d4207d37bfe6a86c6c1366b0dce
elB4RTO <81904638+elB4RTO@users.noreply.github.com>
2022-05-06 20:31:29 +0200

    Update crapview.java
    code improvements

--- craplog/src/main/java/crapcode/crapview.java
+++ craplog/src/main/java/crapcode/crapview.java
@@ -13,6 +13,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import javax.swing.DefaultListModel;
 import javax.swing.JOptionPane;
 import javax.swing.JTable;
@@ -21,31 +22,68 @@ import javax.swing.table.DefaultTableModel;
 public class crapview {
     
     
+    private static void sortList( DefaultListModel<String> logs_list ) {
+        // re-arrange in a sorted list
+        String n1, n2;
+        int m, r;
+        for ( int i=0; i<logs_list.size(); i++ ) {
+            // get the first file name
+            n1 = logs_list.get(i);
+            for ( int j=0; j<logs_list.size(); j++ ) {
+                // get the second file name
+                n2 = logs_list.get(j);
+                // get the shorter name's length
+                m = n1.length();
+                if ( m > n2.length() ) {
+                    m = n2.length();
+                }
+                // compare each character
+                r = 0;
+                for ( int k=0; k<m; k++ ) {
+                    if ( n1.charAt(k) < n2.charAt(k) ) {
+                        // second is smaller
+                        r = 1;
+                        break;
+                    } else if ( n1.charAt(k) > n2.charAt(k) ) {
+                        // firse is smaller
+                        r = -1;
+                        break;
+                    }
+                }
+                // if no difference was found, the shorter comes first
+                if ( r == 0 ) {
+                    if ( n1.length() > n2.length() ) {
+                        r = 1;
+                    }
+                }
+                // switch positions if needed
+                if ( r == 1 ) {
+                    logs_list.set(i, n2);
+                    logs_list.set(j, n1);
+                    n1 = n2;
+                }
+            }
+        }
+    }
+    
+    
     public static String viewLogFile(
         String logs_dir,
         String file_name
     ) {
         // read and return the content
         String read = new String();
-        Path path = Paths.get( String.format("%s/%s",logs_dir,file_name) );
+        Path path = Paths.get( String.format("%s/%s",logs_dir,file_name) ).toAbsolutePath();
         // attempt reading
-        try {
-            InputStream f_in = Files.newInputStream( path );
-            BufferedInputStream buff_in = new BufferedInputStream( f_in );
-            // get data
-            read = new String( buff_in.readAllBytes() );
-            // close
-            buff_in.close();
-            f_in.close();
-        
-        } catch (FileNotFoundException e) {
-            JOptionPane.showMessageDialog(null, String.format("An error occured while searching for log file:\n'%s'",path), "File not found", 0);
-
-        } catch (IOException e) {
-            JOptionPane.showMessageDialog(null, String.format("An error occured while reading log file:\n'%s'",path), "Error reading", 0);
+        HashMap<String,String> proceed = new HashMap<>();
+        proceed.put("state", "true");
+        ArrayList<String> lines = crapcode.craputils.validate.readFile( proceed , path.toString());
+        for ( String line : lines ) {
+            read += String.format("%s",line.replaceAll( "\n", "\n\n" ));
         }
-        
-        return read.replaceAll("\n", "\n\n");
+        proceed.clear();
+        lines.clear();
+        return read;
     }
     
     
@@ -54,37 +92,9 @@ public class crapview {
         DefaultListModel logs_list,
         JTable table_list
     ) {
-        
-        logs_list.clear();
-        File logs_dir = new File( logs_dir_ );
-        // retrieve available sessions
-        if ( logs_dir.exists() ) {
-            FileFilter logFileFilter = (File file) -> file.isFile();
-            for ( File log_file : logs_dir.listFiles(logFileFilter) ) {
-                // all sessions folders (dates)
-                if ( log_file.exists() ) {
-                    // path exists
-                    if ( log_file.isDirectory() ) {
-                        JOptionPane.showMessageDialog(null, String.format("An error occured with:\n'%s'\n\nIt was supposed to be a file, but it was found to be a directory",log_file.getAbsolutePath()), "File-type error", 2);
-                    } else {
-                        String file_name = log_file.getName();
-                        // only append files with default names
-                        if (!file_name.endsWith(".gz")
-                        &&  (file_name.startsWith("access.log")
-                        ||   file_name.startsWith("error.log")) ) {
-                            // valid file-name, may be a valid log file
-                            if ( Files.isReadable(Paths.get( log_file.getAbsolutePath() )) ) {
-                                // is readable, add to list
-                                logs_list.addElement( file_name );
-                            } else {
-                                // warn for unreadable file
-                                JOptionPane.showMessageDialog(null, String.format("Warning on file:\n'%s'\n\nThis file cannot be read.\nIf you was planning to use it,\nplease check permissions and retry",log_file.getAbsolutePath()), "File not readable", 1);
-                            }
-                        }
-                    }
-                }
-            }
-        }
+        // get the available log files
+        logs_list = crapview.getLogsList( logs_dir_ );
+        // make the table
         DefaultTableModel m = new DefaultTableModel();
         m.addColumn( logs_dir_ );
         if ( logs_list.size() == 0 ) {
@@ -92,6 +102,8 @@ public class crapview {
             m.addRow( new Object[] {""} );
 
         } else {
+            // sort elements by name
+            crapview.sortList( logs_list );
             // fill table rows
             for ( int i=0; i<logs_list.size(); i++ ) {
                 m.addRow( new Object[] {logs_list.get(i)} );
@@ -136,47 +148,8 @@ public class crapview {
                 }
             }
         }
-        String n1, n2;
-        int m, r;
-        for ( int i=0; i<logs_list.size(); i++ ) {
-            // get the first file name
-            n1 = logs_list.get(i);
-            for ( int j=0; j<logs_list.size(); j++ ) {
-                // get the second file name
-                n2 = logs_list.get(j);
-                // get the shorter name's length
-                m = n1.length();
-                if ( m > n2.length() ) {
-                    m = n2.length();
-                }
-                // compare each character
-                r = 0;
-                for ( int k=0; k<m; k++ ) {
-                    if ( n1.charAt(k) < n2.charAt(k) ) {
-                        // second is smaller
-                        r = 1;
-                        break;
-                    } else if ( n1.charAt(k) > n2.charAt(k) ) {
-                        // firse is smaller
-                        r = -1;
-                        break;
-                    }
-                }
-                // if no difference was found, the shorter comes first
-                if ( r == 0 ) {
-                    if ( n1.length() > n2.length() ) {
-                        r = 1;
-                    }
-                }
-                // switch positions if needed
-                if ( r == 1 ) {
-                    System.out.println(n1+"   "+n2);
-                    System.out.println(logs_list);
-                    logs_list.set(i, n2);
-                    logs_list.set(j, n1);
-                    n1 = n2;
-                }
-            }
+        if ( !logs_list.isEmpty() ) {
+            crapview.sortList( logs_list );
         }
         return logs_list;
     }
