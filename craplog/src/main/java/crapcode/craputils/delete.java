d4622fc2cb32719023046d8b8d8a14f4d5dd1ec7
elB4RTO <81904638+elB4RTO@users.noreply.github.com>
2022-05-07 22:05:59 +0200

    Update delete.java
    general improvements

--- craplog/src/main/java/crapcode/craputils/delete.java
+++ craplog/src/main/java/crapcode/craputils/delete.java
@@ -1,15 +1,23 @@
 
 package crapcode.craputils;
 
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+
+import java.io.IOException;
 
 import java.util.List;
 import javax.swing.JOptionPane;
 
 public class delete {
+    
+    private static void showPermissionsErrorDialog() {
+        // explaination of the error
+        JOptionPane.showMessageDialog(null,
+            String.format("The error was most likely caused by missing permissions.\nThe process cannot be un-done and will not be aborted now.\nPlease consider to MANUALLY REMOVE THE FILE\nand to change files/folder permissions (USE CAUTION)\nif you're planning to use this function again"),
+            "Error removing original logs file", 0);
+    }
 
     
     public static void toTrash( String logs_dir, List<String> target_files, String trash_dir ) {
@@ -20,8 +28,10 @@ public class delete {
             try {
                 Files.move( file_path, trash_path );
             } catch (IOException e) {
-                JOptionPane.showMessageDialog(null, String.format("An error occured while moving original logs file to trash:\n'%s'",file_path), "Error moving file to trash", 0);
-                JOptionPane.showMessageDialog(null, String.format("The error was most likely caused by missing permissions.\nThe process cannot be un-done and will not be aborted now.\nPlease consider to MANUALLY REMOVE THE FILE\nand to change files/folder permissions (USE CAUTION)\nif you're planning to use this function again",file_path), "Error removing original logs file", 0);
+                JOptionPane.showMessageDialog(null,
+                    String.format("An error occured while moving original logs file to trash:\n'%s'",file_path),
+                    "Error moving file to trash", 0);
+                delete.showPermissionsErrorDialog();
             }
         }
     }
@@ -34,8 +44,10 @@ public class delete {
             try {
                 Files.delete( file_path );
             } catch (IOException e) {
-                JOptionPane.showMessageDialog(null, String.format("An error occured while deleting original logs file to trash:\n'%s'",file_path), "Error deleting file", 0);
-                JOptionPane.showMessageDialog(null, String.format("The error was most likely caused by missing permissions.\nThe process cannot be un-done and will not be aborted now.\nPlease consider to MANUALLY REMOVE THE FILE\nand to change files/folder permissions (USE CAUTION)\nif you're planning to use this function again",file_path), "Error removing original logs file", 0);
+                JOptionPane.showMessageDialog(null,
+                    String.format("An error occured while deleting original logs file:\n'%s'",file_path),
+                    "Error deleting file", 0);
+                delete.showPermissionsErrorDialog();
             }
         }
     }
