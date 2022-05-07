39de2ed53181fbffb8c4499db3ed102e87cf6851
elB4RTO <81904638+elB4RTO@users.noreply.github.com>
2022-05-07 22:06:22 +0200

    Update check.java
    general improvements

--- craplog/src/main/java/crapcode/craputils/check.java
+++ craplog/src/main/java/crapcode/craputils/check.java
@@ -24,14 +24,18 @@ public class check {
                 path = Paths.get(String.format("%s/%s",logs_dir,log_file));
                 if (Files.exists(path)) {
                     if ( !Files.isReadable(path) ) {
-                        JOptionPane.showMessageDialog(null, String.format("An error occured while checking:\n'%s'\n\nThis file is not readable, please check the permissions and retry",path), "Un-readable log file", 0);
+                        JOptionPane.showMessageDialog(null,
+                            String.format("An error occured while checking:\n'%s'\n\nThis file is not readable, please check the permissions and retry",path),
+                            "Un-readable log file", 0);
                         proceed.replace("state", "false");
                         break;
                     }
                 }
             }
         } else {
-            JOptionPane.showMessageDialog(null, String.format("An error occured while checking:\n'%s'\n\nThis folder should contain files, but it does not exists.\nPlease open the settings menu and set-up a valid one.",Paths.get(logs_dir)), "Un-existent stats directory", 0);
+            JOptionPane.showMessageDialog(null,
+                String.format("An error occured while checking:\n'%s'\n\nThis folder should contain log files, but it does not exists.\nPlease open the settings menu and set-up a valid one.",Paths.get(logs_dir)),
+                "Un-existent logs directory", 0);
             proceed.replace("state", "false");
         }
         
@@ -39,11 +43,15 @@ public class check {
             path = Paths.get(stats_dir);
             if (Files.exists(path)) {
                 if ( !Files.isWritable(path) ) {
-                    JOptionPane.showMessageDialog(null, String.format("An error occured while checking:\n'%s'\n\nThis folder is not writable, please check the permissions and retry",path), "Un-writable stats directory", 0);
+                    JOptionPane.showMessageDialog(null,
+                        String.format("An error occured while checking:\n'%s'\n\nThis folder is not writable, please check the permissions and retry",path),
+                        "Un-writable stats directory", 0);
                     proceed.replace("state", "false");
                 }
             } else {
-                JOptionPane.showMessageDialog(null, String.format("An error occured while checking:\n'%s'\n\nThis folder is responsible for holding statistics files,\nbut it does not exists.\nPlease open the settings menu and set-up a valid one",path), "Un-existent stats directory", 0);
+                JOptionPane.showMessageDialog(null,
+                    String.format("An error occured while checking:\n'%s'\n\nThis folder is responsible for holding statistics files,\nbut it does not exists.\nPlease open the settings menu and set-up a valid one",path),
+                    "Un-existent stats directory", 0);
                 proceed.replace("state", "false");
             }
         }
@@ -57,11 +65,15 @@ public class check {
         Path path = Paths.get(trash_dir);
         if (Files.exists(path)) {
             if ( !Files.isWritable(path) ) {
-                JOptionPane.showMessageDialog(null, String.format("An error occured while checking:\n'%s'\n\nThis folder is not writable, please check the permissions and retry",path), "Un-writable trash directory", 0);
+                JOptionPane.showMessageDialog(null,
+                    String.format("An error occured while checking:\n'%s'\n\nThis folder is not writable, please check the permissions and retry",path),
+                    "Un-writable trash directory", 0);
                 proceed.replace("state", "false");
             }
         } else {
-            JOptionPane.showMessageDialog(null, String.format("An error occured while checking:\n'%s'\n\nThe folder doen't exists.\nPlease insert the correct path of your trash in the settings and retry",path), "Missing trash", 0);
+            JOptionPane.showMessageDialog(null,
+                String.format("An error occured while checking:\n'%s'\n\nThe folder doen't exists.\nPlease insert the correct path of your trash in the settings and retry",path),
+                "Un-existent trash directory", 0);
             proceed.replace("state", "false");
         }
     }
